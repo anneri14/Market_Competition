afa1970bfa521aaa33492b340f9c55d624c18115
anneri14 <156306110+anneri14@users.noreply.github.com>
2025-06-17 17:31:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,19 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
-templates = Jinja2Templates(directory="templates")
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app = FastAPI() #создание экземпляра FastAPI приложения
+
+templates = Jinja2Templates(directory="templates") #инициализация шаблонов Jinja2
+app.mount("/static", StaticFiles(directory="static"), name="static") #монтирование статических файлов
 
 from router import authentication, game, pages
 
+#подключение роутеров к основному приложению
 app.include_router(pages.router)
 app.include_router(authentication.router)
 app.include_router(game.router)
 
+#настройка CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
